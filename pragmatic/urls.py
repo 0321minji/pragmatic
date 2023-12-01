4b7ff15b4d32fafe023be0a87828c3e103e0baa6
0321minji <0321minji@ewhain.net>
2023-12-02 01:58:26 +0900

    django course 34 commit

--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -21,7 +21,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accountapp.urls')),
-    path('profiles/',include('profileapp.urls'))
+    path('profiles/',include('profileapp.urls')),
+    path('articles/',include('articleapp.urls')),
     ##accountapp 안의 url을 참고해서 분기됨
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
